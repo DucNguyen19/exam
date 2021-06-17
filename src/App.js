9a00bac213050087367d21573fa893a452e4996f
DucNguyen19 <nguyenduc059912@gmail.com>
2021-06-17 13:09:00 +0700

    modal => true

--- src/App.js
+++ src/App.js
@@ -165,7 +165,7 @@ class App extends Component {
                           
                           <div>
                           <Button style = {{'display': c.author === user.name ? 'block' : 'none', 'float' : 'right'}} onClick={this.toggle}>Edit Post</Button>
-                              <Modal isOpen={modal} toggle={this.toggle}>
+                              <Modal isOpen={true} toggle={this.toggle}>
                                     <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
                                     <ModalBody>
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
