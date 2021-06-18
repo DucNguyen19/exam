0d8d4ee54fd389d070da740624c4e8e0bc4d0b20
DucNguyen19 <nguyenduc059912@gmail.com>
2021-06-18 22:14:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -165,16 +165,7 @@ class App extends Component {
                           
                           <div>
                           <Button style = {{'display': c.author === user.name ? 'block' : 'none', 'float' : 'right'}} onClick={this.toggle}>Edit Post</Button>
-                              <Modal isOpen={modal} toggle={this.toggle}>
-                                    <ModalHeader toggle={this.toggle}>Modal Title</ModalHeader>
-                                    <ModalBody>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    </ModalBody>
-                                    <ModalFooter>
-                                    <Button color="primary" onClick={this.toggle}>Update</Button>{' '}
-                                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                                    </ModalFooter>
-                              </Modal>
+                              
                         </div>
                     </Col>
                     
@@ -201,7 +192,18 @@ class App extends Component {
                         <Container>
                           {item}
                         </Container>
-                        
+                        <div>
+                              <Modal isOpen={modal} toggle={this.toggle}>
+                                    <ModalHeader toggle={this.toggle}>Modal Title</ModalHeader>
+                                    <ModalBody>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </ModalBody>
+                                    <ModalFooter>
+                                    <Button color="primary" onClick={this.toggle}>Update</Button>{' '}
+                                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                                    </ModalFooter>
+                              </Modal>
+                        </div>
                   </div>
                   
             );
